980b833cf0ae2eb5bc3f67e87fcfeb2d1c30edce
Michael Lee <hi@michaelsoolee.com>
2023-03-15 21:16:34 -0400

    Hooked up logic for toggling

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 import { App, addIcon, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const nameOfApplication = `Soomda`;
 
 interface SoomdaPluginSettings {
-	mySetting: string;
+	both: boolean;
+	justLeft: boolean;
+	justRight: boolean;
 }
 
 const DEFAULT_SETTINGS: SoomdaPluginSettings = {
-	mySetting: 'default'
+	both: false,
+	justLeft: false,
+	justRight: false
 }
 
 const icon = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Leaf</title><path fill="currentColor" d="M161.35 242a16 16 0 0122.62-.68c73.63 69.36 147.51 111.56 234.45 133.07 11.73-32 12.77-67.22 2.64-101.58-13.44-45.59-44.74-85.31-90.49-114.86-40.84-26.38-81.66-33.25-121.15-39.89-49.82-8.38-96.88-16.3-141.79-63.85-5-5.26-11.81-7.37-18.32-5.66-7.44 2-12.43 7.88-14.82 17.6-5.6 22.75-2 86.51 13.75 153.82 25.29 108.14 65.65 162.86 95.06 189.73 38 34.69 87.62 53.9 136.93 53.9a186 186 0 0027.77-2.04c41.71-6.32 76.43-27.27 96-57.75-89.49-23.28-165.94-67.55-242-139.16a16 16 0 01-.65-22.65zM467.43 384.19c-16.83-2.59-33.13-5.84-49-9.77a157.71 157.71 0 01-12.13 25.68c-.73 1.25-1.5 2.49-2.29 3.71a584.21 584.21 0 0058.56 12 16 16 0 104.87-31.62z"/></svg>`
@@ -26,23 +30,25 @@ export default class SoomdaPlugin extends Plugin {
 			this.togglePanes();
 		});
 
-		// This adds a simple command that can be triggered anywhere
+		await this.checkSidebars()
+
+		// Adds command to show sidebars
 		this.addCommand({
 			id: 'soomda-show-sidebars',
 			name: 'Show sidebars',
 			callback: () => {
-				this.togglePanes();
+				this.showSidebars();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		// Adds command to hide sidebars
 		this.addCommand({
 			id: 'soomda-hide-sidebars',
 			name: 'Hide sidebars',
 			callback: () => {
-				this.togglePanes();
+				this.hideSidebars();
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		// Adds command to toggle sidebars
 		this.addCommand({
 			id: 'soomda-toggle-sidebars',
 			name: 'Toggle sidebars',
@@ -68,6 +74,7 @@ export default class SoomdaPlugin extends Plugin {
 	}
 
 	async hideSidebars() {
+		await this.checkSidebars()
 		const workspace = this.app.workspace
 		const leftSplit = workspace.leftSplit
 		const rightSplit = workspace.rightSplit
@@ -83,20 +90,70 @@ export default class SoomdaPlugin extends Plugin {
 		
 		leftSplit.expand()
 		rightSplit.expand()
+		
 	}
 
 	async togglePanes() {
 		const workspace = this.app.workspace
 		const leftSplit = workspace.leftSplit
 		const rightSplit = workspace.rightSplit
+		
+		const currentBoth = this.settings.both
+		const currentJustLeft = this.settings.justLeft
+		const currentJustRight = this.settings.justRight
 
-		if (leftSplit.collapsed && rightSplit.collapsed) {
-			leftSplit.expand()
-			rightSplit.expand()
-		} else {
+		await this.checkSidebars()
+
+		if (!leftSplit.collapsed || !rightSplit.collapsed) {
 			leftSplit.collapse()
 			rightSplit.collapse()
+			return
 		}
+
+		if (currentBoth) {
+			leftSplit.expand()
+			rightSplit.expand()
+			return
+		}
+
+		if (currentJustLeft) {
+			leftSplit.expand()
+			rightSplit.collapse()
+			return
+		}
+
+		if (currentJustRight) {
+			leftSplit.collapse()
+			rightSplit.expand()
+			return
+		}
+
+	}
+
+	async checkSidebars() {
+		const workspace = this.app.workspace
+		const leftSplit = workspace.leftSplit
+		const rightSplit = workspace.rightSplit
+		
+		if (!leftSplit.collapsed && !rightSplit.collapsed) {
+			this.settings.both = true
+			this.settings.justLeft = false
+			this.settings.justRight = false
+		}
+
+		if (leftSplit.collapsed && !rightSplit.collapsed) {
+			this.settings.justRight = true
+			this.settings.justLeft = false
+			this.settings.both = false
+		}
+
+		if (rightSplit.collapsed && !leftSplit.collapsed) {
+			this.settings.justRight = false
+			this.settings.justLeft = true
+			this.settings.both = false
+		}
+
+		await this.saveSettings();
 	}
 }
 
@@ -109,22 +166,13 @@ class SoomdaSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		containerEl.createEl("h3", {
+			text: `${nameOfApplication} — Quickly hide your sidebars.`,
+		});
+		containerEl.createEl("h4", {
+			text: "v" + this.plugin.manifest.version,
+		});
 	}
 }
