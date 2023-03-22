877e951c0346497638d980568479a66cb8a2c183
Michael Lee <hi@michaelsoolee.com>
2023-03-21 23:23:18 -0400

    Used prettier to format code

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, addIcon, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, addIcon, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 const nameOfApplication = `Soomda`;
 
@@ -13,10 +13,10 @@ const DEFAULT_SETTINGS: SoomdaPluginSettings = {
 	both: true,
 	justLeft: false,
 	justRight: false,
-	rememberSidebar: false
-}
+	rememberSidebar: false,
+};
 
-const soomdaIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Leaf</title><path fill="currentColor" d="M161.35 242a16 16 0 0122.62-.68c73.63 69.36 147.51 111.56 234.45 133.07 11.73-32 12.77-67.22 2.64-101.58-13.44-45.59-44.74-85.31-90.49-114.86-40.84-26.38-81.66-33.25-121.15-39.89-49.82-8.38-96.88-16.3-141.79-63.85-5-5.26-11.81-7.37-18.32-5.66-7.44 2-12.43 7.88-14.82 17.6-5.6 22.75-2 86.51 13.75 153.82 25.29 108.14 65.65 162.86 95.06 189.73 38 34.69 87.62 53.9 136.93 53.9a186 186 0 0027.77-2.04c41.71-6.32 76.43-27.27 96-57.75-89.49-23.28-165.94-67.55-242-139.16a16 16 0 01-.65-22.65zM467.43 384.19c-16.83-2.59-33.13-5.84-49-9.77a157.71 157.71 0 01-12.13 25.68c-.73 1.25-1.5 2.49-2.29 3.71a584.21 584.21 0 0058.56 12 16 16 0 104.87-31.62z"/></svg>`
+const soomdaIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Leaf</title><path fill="currentColor" d="M161.35 242a16 16 0 0122.62-.68c73.63 69.36 147.51 111.56 234.45 133.07 11.73-32 12.77-67.22 2.64-101.58-13.44-45.59-44.74-85.31-90.49-114.86-40.84-26.38-81.66-33.25-121.15-39.89-49.82-8.38-96.88-16.3-141.79-63.85-5-5.26-11.81-7.37-18.32-5.66-7.44 2-12.43 7.88-14.82 17.6-5.6 22.75-2 86.51 13.75 153.82 25.29 108.14 65.65 162.86 95.06 189.73 38 34.69 87.62 53.9 136.93 53.9a186 186 0 0027.77-2.04c41.71-6.32 76.43-27.27 96-57.75-89.49-23.28-165.94-67.55-242-139.16a16 16 0 01-.65-22.65zM467.43 384.19c-16.83-2.59-33.13-5.84-49-9.77a157.71 157.71 0 01-12.13 25.68c-.73 1.25-1.5 2.49-2.29 3.71a584.21 584.21 0 0058.56 12 16 16 0 104.87-31.62z"/></svg>`;
 
 export default class SoomdaPlugin extends Plugin {
 	settings: SoomdaPluginSettings;
@@ -24,51 +24,57 @@ export default class SoomdaPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		addIcon('icon', soomdaIcon);
+		addIcon("icon", soomdaIcon);
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('icon', 'Toggle Soomda', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			this.togglePanes();
-		});
+		const ribbonIconEl = this.addRibbonIcon(
+			"icon",
+			"Toggle Soomda",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				this.togglePanes();
+			}
+		);
 
-		await this.checkSidebars()
+		await this.checkSidebars();
 
 		// Adds command to show sidebars
 		this.addCommand({
-			id: 'show-sidebars',
-			name: 'Show sidebars',
+			id: "show-sidebars",
+			name: "Show sidebars",
 			callback: () => {
 				this.showSidebars();
-			}
+			},
 		});
 		// Adds command to hide sidebars
 		this.addCommand({
-			id: 'hide-sidebars',
-			name: 'Hide sidebars',
+			id: "hide-sidebars",
+			name: "Hide sidebars",
 			callback: () => {
 				this.hideSidebars();
-			}
+			},
 		});
 		// Adds command to toggle sidebars
 		this.addCommand({
-			id: 'toggle-sidebars',
-			name: 'Toggle sidebars',
+			id: "toggle-sidebars",
+			name: "Toggle sidebars",
 			callback: () => {
 				this.togglePanes();
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SoomdaSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -76,93 +82,90 @@ export default class SoomdaPlugin extends Plugin {
 	}
 
 	async hideSidebars() {
-		const workspace = this.app.workspace
-		const leftSplit = workspace.leftSplit
-		const rightSplit = workspace.rightSplit
-		
-		leftSplit.collapse()
-		rightSplit.collapse()
+		const workspace = this.app.workspace;
+		const leftSplit = workspace.leftSplit;
+		const rightSplit = workspace.rightSplit;
+
+		leftSplit.collapse();
+		rightSplit.collapse();
 	}
 
 	async showSidebars() {
-		const workspace = this.app.workspace
-		const leftSplit = workspace.leftSplit
-		const rightSplit = workspace.rightSplit
-		
-		leftSplit.expand()
-		rightSplit.expand()
-		
+		const workspace = this.app.workspace;
+		const leftSplit = workspace.leftSplit;
+		const rightSplit = workspace.rightSplit;
+
+		leftSplit.expand();
+		rightSplit.expand();
 	}
 
 	async togglePanes() {
-		const workspace = this.app.workspace
-		const leftSplit = workspace.leftSplit
-		const rightSplit = workspace.rightSplit
-		
-		const currentBoth = this.settings.both
-		const currentJustLeft = this.settings.justLeft
-		const currentJustRight = this.settings.justRight
+		const workspace = this.app.workspace;
+		const leftSplit = workspace.leftSplit;
+		const rightSplit = workspace.rightSplit;
+
+		const currentBoth = this.settings.both;
+		const currentJustLeft = this.settings.justLeft;
+		const currentJustRight = this.settings.justRight;
 
 		if (this.settings.rememberSidebar) {
-		
-			await this.checkSidebars()
+			await this.checkSidebars();
 
 			if (!leftSplit.collapsed || !rightSplit.collapsed) {
-				leftSplit.collapse()
-				rightSplit.collapse()
-				return
+				leftSplit.collapse();
+				rightSplit.collapse();
+				return;
 			}
 
 			if (currentBoth) {
-				leftSplit.expand()
-				rightSplit.expand()
-				return
+				leftSplit.expand();
+				rightSplit.expand();
+				return;
 			}
 
 			if (currentJustLeft) {
-				leftSplit.expand()
-				rightSplit.collapse()
-				return
+				leftSplit.expand();
+				rightSplit.collapse();
+				return;
 			}
 
 			if (currentJustRight) {
-				leftSplit.collapse()
-				rightSplit.expand()
-				return
+				leftSplit.collapse();
+				rightSplit.expand();
+				return;
 			}
 		} else {
 			if (leftSplit.collapsed && rightSplit.collapsed) {
-				leftSplit.expand()
-				rightSplit.expand()
+				leftSplit.expand();
+				rightSplit.expand();
 			} else {
-				leftSplit.collapse()
-				rightSplit.collapse()
+				leftSplit.collapse();
+				rightSplit.collapse();
 			}
 		}
-
 	}
 
 	async checkSidebars() {
-		const workspace = this.app.workspace
-		const leftSplit = workspace.leftSplit
-		const rightSplit = workspace.rightSplit
-		
+		const workspace = this.app.workspace;
+		const leftSplit = workspace.leftSplit;
+		const rightSplit = workspace.rightSplit;
+
 		if (!leftSplit.collapsed && !rightSplit.collapsed) {
-			this.settings.both = true
-			this.settings.justLeft = false
-			this.settings.justRight = false
+			this.settings.both = true;
+			this.settings.justLeft = false;
+			this.settings.justRight = false;
 		}
 
 		if (leftSplit.collapsed && !rightSplit.collapsed) {
-			this.settings.justRight = true
-			this.settings.justLeft = false
-			this.settings.both = false
+			this.settings.justRight = true;
+			this.settings.justLeft = false;
+			this.settings.both = false;
 		}
 
 		if (rightSplit.collapsed && !leftSplit.collapsed) {
-			this.settings.justRight = false
-			this.settings.justLeft = true
-			this.settings.both = false
+			this.settings.justRight = false;
+			this.settings.justLeft = true;
+			this.settings.both = false;
 		}
 
 		await this.saveSettings();
@@ -206,8 +209,11 @@ class SoomdaSettingTab extends PluginSettingTab {
 				title
 					.setValue(this.plugin.settings.rememberSidebar)
 					.onChange(async () => {
-						this.plugin.settings.rememberSidebar = this.plugin.settings.rememberSidebar ? false : true
-						await this.plugin.saveSettings()
+						this.plugin.settings.rememberSidebar = this.plugin
+							.settings.rememberSidebar
+							? false
+							: true;
+						await this.plugin.saveSettings();
 					})
 			);
 	}
