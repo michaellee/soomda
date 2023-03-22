e23dcbc2bf7366e036d57fa26ad780d77f587fd0
Michael Lee <hi@michaelsoolee.com>
2023-03-21 23:14:28 -0400

    Updates based on feedback from PR

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ const DEFAULT_SETTINGS: SoomdaPluginSettings = {
 	rememberSidebar: false
 }
 
-const icon = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Leaf</title><path fill="currentColor" d="M161.35 242a16 16 0 0122.62-.68c73.63 69.36 147.51 111.56 234.45 133.07 11.73-32 12.77-67.22 2.64-101.58-13.44-45.59-44.74-85.31-90.49-114.86-40.84-26.38-81.66-33.25-121.15-39.89-49.82-8.38-96.88-16.3-141.79-63.85-5-5.26-11.81-7.37-18.32-5.66-7.44 2-12.43 7.88-14.82 17.6-5.6 22.75-2 86.51 13.75 153.82 25.29 108.14 65.65 162.86 95.06 189.73 38 34.69 87.62 53.9 136.93 53.9a186 186 0 0027.77-2.04c41.71-6.32 76.43-27.27 96-57.75-89.49-23.28-165.94-67.55-242-139.16a16 16 0 01-.65-22.65zM467.43 384.19c-16.83-2.59-33.13-5.84-49-9.77a157.71 157.71 0 01-12.13 25.68c-.73 1.25-1.5 2.49-2.29 3.71a584.21 584.21 0 0058.56 12 16 16 0 104.87-31.62z"/></svg>`
+const soomdaIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Leaf</title><path fill="currentColor" d="M161.35 242a16 16 0 0122.62-.68c73.63 69.36 147.51 111.56 234.45 133.07 11.73-32 12.77-67.22 2.64-101.58-13.44-45.59-44.74-85.31-90.49-114.86-40.84-26.38-81.66-33.25-121.15-39.89-49.82-8.38-96.88-16.3-141.79-63.85-5-5.26-11.81-7.37-18.32-5.66-7.44 2-12.43 7.88-14.82 17.6-5.6 22.75-2 86.51 13.75 153.82 25.29 108.14 65.65 162.86 95.06 189.73 38 34.69 87.62 53.9 136.93 53.9a186 186 0 0027.77-2.04c41.71-6.32 76.43-27.27 96-57.75-89.49-23.28-165.94-67.55-242-139.16a16 16 0 01-.65-22.65zM467.43 384.19c-16.83-2.59-33.13-5.84-49-9.77a157.71 157.71 0 01-12.13 25.68c-.73 1.25-1.5 2.49-2.29 3.71a584.21 584.21 0 0058.56 12 16 16 0 104.87-31.62z"/></svg>`
 
 export default class SoomdaPlugin extends Plugin {
 	settings: SoomdaPluginSettings;
@@ -24,7 +24,7 @@ export default class SoomdaPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		addIcon('icon', icon);
+		addIcon('icon', soomdaIcon);
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('icon', 'Toggle Soomda', (evt: MouseEvent) => {
@@ -36,7 +36,7 @@ export default class SoomdaPlugin extends Plugin {
 
 		// Adds command to show sidebars
 		this.addCommand({
-			id: 'soomda-show-sidebars',
+			id: 'show-sidebars',
 			name: 'Show sidebars',
 			callback: () => {
 				this.showSidebars();
@@ -44,7 +44,7 @@ export default class SoomdaPlugin extends Plugin {
 		});
 		// Adds command to hide sidebars
 		this.addCommand({
-			id: 'soomda-hide-sidebars',
+			id: 'hide-sidebars',
 			name: 'Hide sidebars',
 			callback: () => {
 				this.hideSidebars();
@@ -52,7 +52,7 @@ export default class SoomdaPlugin extends Plugin {
 		});
 		// Adds command to toggle sidebars
 		this.addCommand({
-			id: 'soomda-toggle-sidebars',
+			id: 'toggle-sidebars',
 			name: 'Toggle sidebars',
 			callback: () => {
 				this.togglePanes();
